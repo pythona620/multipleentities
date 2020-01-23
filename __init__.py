2c903d94c2cb77c2611ac3e04d6ba52ce498266e
pythona620 <59389520+pythona620@users.noreply.github.com>
2020-01-23 18:19:07 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,19 +5,18 @@ class TomatoSkill(MycroftSkill):
         MycroftSkill.__init__(self)
 
     def initialize(self):
+        self.register_intent_file('i.am.intent', self.handle_.i_am) #register the intentes
         self.register_intent_file('where.are.intent', self.handle_where_are) #register the intentes
-        self.register_intent_file('i.am.intent', self.handle_i_am) #register the intentes
 
     def handle_what_is(self, message): #get the intent
-        self.speak('your friend is going') 
+        self.speak('A tomato is a big red thing') #
 
-#     def handle_do_you_like(self, message):
-#         going_from = message.data.get('type1')  #get the specific keword into type
-#         going = message.data.get('type2') 
-#         if tomato_type is not None:
-#             self.speak("Well, your specific keyword is " + "going_from" + "going" + " in your word.") #print specific keword
-#         else:
-#             self.speak('you are not given any specific word!') #print if specific keword is not given
+    def handle_do_you_like(self, message):
+        tomato_type = message.data.get('type')  #get the specific keword into type
+        if tomato_type is not None:
+            self.speak("Well, your specific keyword is " + tomato_type + " in your word.") #print specific keword
+        else:
+            self.speak('you are not given any specific word!') #print if specific keword is not given
 
 
     def stop(self):
