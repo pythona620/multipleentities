9f4afdcce90493b8587a1ce3f6662968400ab58a
pythona620 <59389520+pythona620@users.noreply.github.com>
2020-01-24 15:24:55 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from mycroft import MycroftSkill, intent_file_handler
 
-class TomatoSkill(MycroftSkill):
+class multipleentitiesSkill(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
@@ -8,14 +8,14 @@ class TomatoSkill(MycroftSkill):
         self.register_intent_file('what.is.intent', self.handle_what_is) #register the intentes
         self.register_intent_file('your.friend.intent', self.handle_do_you_like) #register the intentes
 
-    def handle_what_is(self, message): #get the intent
-        self.speak('A tomato is a big red thing') #
+    def handle_what_is(self, message): 
+        self.speak('your friend is going to some place') #not given place
 
     def handle_do_you_like(self, message):
-        tomato_type = message.data.get('condition')  #get the specific keword into type
-        tomato = message.data.get('temp')
+        first_type = message.data.get('from')  #get the first keword
+        second_type = message.data.get('to') #get the second keword
         if tomato_type is not None:
-            self.speak("your friend is going " + tomato_type + "to" + tomato ) #print specific keword
+            self.speak("your friend is going " + first_type  + " " + "to" + " "+  second_type ) #print specific keword
         else:
             self.speak('you are not given any specific word!') #print if specific keword is not given
 
@@ -24,4 +24,4 @@ class TomatoSkill(MycroftSkill):
         pass
 
 def create_skill():
-    return TomatoSkill()
+    return multipleentitiesSkill()
